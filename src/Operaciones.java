25c1b1fadae3facd04448d45001ff9c74e011e7f
FakeWing <carlos.moralesu@usach.cl>
2019-09-25 16:20:31 -0300

    update clase operaciones

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -1,28 +1,32 @@
 
 public class Operaciones {
-	
-	
+
+	// trabajando en la rama slave
+
 	public int sumar(int A, int B) {
-		if (A==0 || B==0) {
+		if (A == 0 || B == 0) {
 			return 0;
-		}else {
-			return A+B;
+		} else {
+			return A + B;
 		}
-		
-	}
-	
-	public int restar(int A, int B) {
-		
-		return A-B;
-	}
-	
-	public int multiplicar (int A, int B) {
-		return A*B;
-	}
-	
-	public int dividir (int A, int B) {
-		return A/B;
+
 	}
 
-	
+	public int restar(int A, int B) {
+		if (B > A) {
+			return 0;
+		} else {
+			return A - B;
+		}
+
+	}
+
+	public int multiplicar(int A, int B) {
+		return A * B;
+	}
+
+	public int dividir(int A, int B) {
+		return A / B;
+	}
+
 }
