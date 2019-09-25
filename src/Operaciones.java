fe39ed169b0760f03a0c620cc7cc1e1b44972d4a
FakeWing <carlos.moralesu@usach.cl>
2019-09-25 15:46:07 -0300

    primer commit

---
+++ src/Operaciones.java
@@ -0,0 +1,28 @@
+
+public class Operaciones {
+	
+	
+	public int sumar(int A, int B) {
+		if (A==0 || B==0) {
+			return 0;
+		}else {
+			return A+B;
+		}
+		
+	}
+	
+	public int restar(int A, int B) {
+		
+		return A-B;
+	}
+	
+	public int multiplicar (int A, int B) {
+		return A*B;
+	}
+	
+	public int dividir (int A, int B) {
+		return A/B;
+	}
+
+	
+}
